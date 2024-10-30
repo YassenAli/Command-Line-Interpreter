2e513c4fa9356184f50532da6d86561d7c4365a0
YassenAli <144853147+YassenAli@users.noreply.github.com>
2024-10-30 22:08:42 +0300

    solve some problems

--- src/main/java/org/os/commands/MkdirCommand.java
+++ src/main/java/org/os/commands/MkdirCommand.java
@@ -17,21 +17,22 @@ public class MkdirCommand implements Command {
 
         String directoryName = args[0];
         Path path = Paths.get(Main.currentDirectory);
+        Path newDirectoryPath = path.resolve(directoryName);
 
         // Check if the parent directory exists
-        if (path.getParent() != null && !Files.exists(path.getParent())) {
+        if (newDirectoryPath.getParent() != null && !Files.exists(newDirectoryPath.getParent())) {
             System.out.println("Parent directory does not exist: " + path.getParent());
             return;
         }
 
         // Check if a file with the same name exists
-        if (Files.exists(path) && !Files.isDirectory(path)) {
-            System.out.println("A file with the same name exists: " + path.getFileName());
+        if (Files.exists(newDirectoryPath) && !Files.isDirectory(newDirectoryPath)) {
+            System.out.println("A file with the same name exists: " + newDirectoryPath.getFileName());
             return;
         }
 
         try {
-            Files.createDirectory(path);
+            Files.createDirectory(newDirectoryPath);
             System.out.println("Directory created: " + path.toString());
         } catch (IOException e) {
             System.out.println("Error creating directory: " + e.getMessage());
